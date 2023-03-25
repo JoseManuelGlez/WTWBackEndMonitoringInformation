09b5cb9587710c9c69475e42562e42a775d533aa
JoseManuelGlez <213505@ids.upchiapas.edu.mx>
2023-03-24 18:17:05 -0600

    ultimas correcciones

--- src/main/java/com/example/wtwbackendmonitoringinformation/repositories/IMonitoringInformationRepository.java
+++ src/main/java/com/example/wtwbackendmonitoringinformation/repositories/IMonitoringInformationRepository.java
@@ -8,6 +8,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMonitoringInformationRepository extends JpaRepository<MonitoringInformation, Long> {
-    @Query(value = "SELECT * FROM monitoring_information WHERE monitoring_information.esp_id = :espId", nativeQuery = true)
+    @Query(value = "SELECT monitoring_information.id AS monitoringInformationId, " +
+            "monitoring_information.date AS monitoringInformationDate," +
+            "monitoring_information.hour AS monitoringInformationHour, " +
+            "monitoring_information.soil_temperature AS monitoringInformationSoilTemperature, " +
+            "monitoring_information.soil_humidity AS monitoringInformationSoilHumidity, " +
+            "monitoring_information.ambient_temperature AS monitoringInformationAmbientTemperature, " +
+            "monitoring_information.ambient_humidity AS monitoringInformationAmbientHumidity, " +
+            "monitoring_information.esp_id AS monitoringInformationEspId " +
+            "FROM monitoring_information " +
+            "WHERE monitoring_information.esp_id = :espId ;", nativeQuery = true)
     IMonitoringInformationProjection findByEsp32Id(Long espId);
 }
